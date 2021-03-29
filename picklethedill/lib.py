3f7d3275396f024b2e365971e547648ffabb56cd
Rex Elardo <rexelardo@gmail.com>
2021-03-29 16:47:13 -0400

    Heroku Procfile added

--- picklethedill/lib.py
+++ picklethedill/lib.py
@@ -1,7 +1,7 @@
 
 
 def makingpickles():
-    a= print('To make good pickles, the secret is in the brine')
+    a= 'To make good pickles, the secret is in the brine'
     return a
 
 
@@ -9,4 +9,4 @@ def try_me():
     a = print('I am trying to make good pickles right now')
     return a
 
-    
+
---
+++ tests/check_words.py
@@ -0,0 +1,5 @@
+# tests/test_lib.py
+from picklethedill.lib import makingpickles
+
+def test_that_it_is_positive():
+    assert type(makingpickles) != int
