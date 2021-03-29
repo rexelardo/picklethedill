30eb87cc19c5e66c05e19a9605d5a5ae0c7eeca1
Rex Elardo <rexelardo@gmail.com>
2021-03-29 14:12:51 -0400

    adding something to my Github repo

---
+++ picklethedill/lib.py
@@ -0,0 +1,12 @@
+
+
+def makingpickles():
+    a= print('To make good pickles, the secret is in the brine')
+    return a
+
+
+def try_me():
+    a = print('I am trying to make good pickles right now')
+    return a
+
+    
